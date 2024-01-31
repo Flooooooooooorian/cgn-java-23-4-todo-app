03733b49f976073826143cd4ac7bf79701d711ce
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2024-01-31 12:47:38 +0100

    Add OAuth2AuthenticationToken

--- backend/src/main/java/de/neuefische/backend/security/UserController.java
+++ backend/src/main/java/de/neuefische/backend/security/UserController.java
@@ -1,20 +1,24 @@
 package de.neuefische.backend.security;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
     @GetMapping("/me")
-    public String getCurrentUser(@AuthenticationPrincipal Principal principal) {
-        return SecurityContextHolder.getContext().getAuthentication().getName();
-        //return principal.getName();
+    public String getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth instanceof OAuth2AuthenticationToken token) {
+            return token.getPrincipal().getAttributes().get("login").toString();
+        }
+
+        return auth.getName();
     }
 }
